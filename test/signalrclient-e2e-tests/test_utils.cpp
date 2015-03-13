683df6db2805f15e32bf939ed19ff6a6ee8178fc
Xiaohongt <xiaota@microsoft.com>
2015-03-12 19:32:15 -0700

    add c++ e2e tests

---
+++ test/signalrclient-e2e-tests/test_utils.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+#pragma once
+
+#include "stdafx.h"
+#include <string>
+#include "cpprest\details\basic_types.h"
+
+utility::string_t get_url(int argc, _TCHAR* argv[])
+{
+	utility::string_t url(U("http://localhost:8081/"));
+
+    for (int i = 0; i < argc; ++i)
+	{
+		utility::string_t str = argv[i];
+
+		if (str.find(U("url=")) != std::string::npos)
+		{
+			size_t pos = str.find(U("url="));
+			url = str.substr(pos + 4);
+			break;
+		}
+	}
+
+	return url;
+}
---
+++ test/signalrclient-testhost/Connections/HubConnection.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace SelfHost
+{
+    public class HubConnection : Hub
+    {
+        public IEnumerable<int> ForceReconnect()
+        {
+            yield return 1;
+            // throwing here will close the websocket which should trigger reconnect
+            throw new Exception();
+        }
+
+        public string DisplayMessage(string message)
+        {
+            Clients.Caller.displayMessage("Send: " + message);
+            return message;
+        }
+    }
+}
